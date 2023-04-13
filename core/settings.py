579924cabb3ab22bdaec4714e4b3d374f9ff3e3c
breef-droid <59095457+breef-droid@users.noreply.github.com>
2023-04-13 12:05:33 +0100

    updates for heroku deployment

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-e_h4piowv*u_5931v3$jl7$99hgfb$!$c&$l8er64o^8*73t#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
--- posts/tests.py
+++ posts/tests.py
@@ -11,13 +11,13 @@ class PostTests(TestCase):
 
     def test_model_content(self):
         self.assertEqual(self.post.text, "This is a test!")
-    
+
     def test_url_exists_at_correct_location(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
 
     def test_homepage(self):
-        response = self.client.get(reverse('home'))
+        response = self.client.get(reverse("home"))
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertTemplateUsed(response, "home.html")
         self.assertContains(response, "This is a test!")
