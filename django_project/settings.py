e8fb917cec6b8ac0299ed247dfa2cde712c4573e
breef-droid <59095457+breef-droid@users.noreply.github.com>
2023-04-13 12:20:21 +0100

    settings file rename

--- django_project/settings.py
+++ django_project/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'core.urls'
+ROOT_URLCONF = 'django_project.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'core.wsgi.application'
+WSGI_APPLICATION = 'django_project.wsgi.application'
 
 
 # Database
